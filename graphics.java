bb24bf471d35e1e154fa3f20e57f5bd19b470aaf
lpeterson52 <lucasp52559@gmail.com>
2023-05-22 22:11:53 -0700

    graphics work

--- graphics.java
+++ graphics.java
@@ -1,52 +1,54 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Point;
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.*;
+// import javax.swing.SwingUtilities;
+// import javax.swing.JFrame;
+// import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.JLabel;
 
 /**
  * A panel maintaining a picture of a Do Not Enter sign.
  */
- public class graphics extends JPanel {
+public class graphics extends JPanel implements MouseListener {
     private static final long serialVersionUID = 7148504528835036003L;
+    Minesweeper m = new Minesweeper();
+    boolean flagging = false;
 
     /**
      * Called by the runtime system whenever the panel needs painting.
      */
+    
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int x = 20;
-        int y;
-        int width = 40; 
-        int height = 40;
+        JLabel label = new JLabel();
+        label.setBounds(20 + 40 * 0,0 * 40 + 20,40,40);
+        String s = "" + m.getGrid(0,0).getNumber();
+        label.setText(s);
+        label.setBackground(Color.GRAY);
+        label.setOpaque(true);
+        label.addMouseListener(this);
+        this.add(label);
+        for (int l = 0; l < 10; l++) {
+            for (int w = 0; w < 10; w++) {
+                // TileGraphics t = new TileGraphics(20 + 40 * l, w * 40 + 20, m.getGrid(l, w).getNumber(), g,
+                //         m.getGrid(l, w).isRevealed());
+                        // t.addMouseListener(this);
+                        // t.setBounds(20 + 40 * l, w * 40 + 20,60+40*l,60+40*w);
+                        // JLabel label = new JLabel();
+                        // label.setBounds(20 + 40 * l,w * 40 + 20,40,40);
+                        // String s = "" + m.getGrid(l,w).getNumber();
+                        // label.setText(s);
+                        // label.setBackground(Color.GRAY);
+                        // label.setOpaque(true);
+                        // label.addMouseListener(this);
+                        // this.add(label);
+                        
+            
 
-
-        // var center = new Point(getWidth() / 2, getHeight() / 2);
-        // var radius = Math.min(getWidth() / 2, getHeight() / 2) - 5;
-        // var diameter = radius * 2;
-        // var innerRadius = (int)(radius * 0.9);
-        // var innerDiameter = innerRadius * 2;
-        // var barWidth = (int)(innerRadius * 1.4);
-        // var barHeight = (int)(innerRadius * 0.35);
-
-
-        // g.drawRect(int x, int y, int width, int height)
-        for(int l =0;l != 11; l++){
-            for(int w = 0; w!= 11;w++){
-            g.drawRect(20 + 40 * l, w * 40 + 20, width, height);
+            }
         }
-    }
-        
-
-        
-        // g.setColor(Color.WHITE);
-        // g.fillOval(center.x - radius, center.y - radius, diameter, diameter);
-        // g.setColor(Color.RED);
-        // g.fillOval(center.x - innerRadius, center.y - innerRadius, innerDiameter, innerDiameter);
-        // g.setColor(Color.WHITE);
-        // g.fillRect(center.x - barWidth/2, center.y - barHeight/2, barWidth, barHeight);
     }
 
     /**
@@ -55,12 +57,33 @@ import javax.swing.JPanel;
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             graphics panel = new graphics();
-            panel.setBackground(Color.GREEN.darker());
-            JFrame frame = new JFrame("A simple graphics program");
-            frame.setSize(500, 600);
+            panel.setBackground(Color.WHITE);
+            JFrame frame = new JFrame("Minesweeper");
+            frame.setSize(460, 600);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.getContentPane().add(panel, BorderLayout.CENTER);
             frame.setVisible(true);
         });
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("1");
+    }   
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
     }
 }
